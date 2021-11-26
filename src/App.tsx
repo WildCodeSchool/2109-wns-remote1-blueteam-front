7d6478b5080b05a5c8a117c3f365da4c99cf83f4
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2021-11-26 11:32:00 +0100

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Layout from './components/layout/Layout';
 
 function App(): JSX.Element {
   
   return (
-    <div className="App" >Hello World</div>
+    <div className="App" >
+      <>Hello World</>
+      <Layout />
+    </div>
   );
 }
 
