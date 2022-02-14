ca9045d293f2b9f85dd0b73a114d36532e0de7da
herve <herve.meste@gmail.com>
2022-02-14 12:46:51 +0100

    add typography for resolve problems the tag cannot be P

--- src/components/firstpage/MyFirstPage.tsx
+++ src/components/firstpage/MyFirstPage.tsx
@@ -1,6 +1,10 @@
+import Typography from '@mui/material/Typography';
 import React, { FC } from 'react';
 import IPage from '../../interfaces/pages';
 
-const MyFirstPage = (props: IPage) => <p>First component works !</p>;
-
+const MyFirstPage = (props: IPage) => (
+    <Typography component="span" variant="body2" style={{ display: 'flex' }}>
+        First component works !
+    </Typography>
+);
 export default MyFirstPage;
