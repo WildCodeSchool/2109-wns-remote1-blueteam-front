446a3aa22e5fbdf1c9ab1d993d3a084fd455aebb
herve <herve.meste@gmail.com>
2022-02-14 14:02:28 +0100

    add signin path and signup path in Router.tsx

--- src/components/routes/Router.tsx
+++ src/components/routes/Router.tsx
@@ -2,6 +2,9 @@ import React, { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from '../dashboard/Dashboard';
 import MyFirstPage from '../firstpage/MyFirstPage';
+import SignIn from '../signin/SignIn';
+import SignUp from '../signup/SignUp';
+
 
 import Layout from '../layout/Layout';
 
@@ -10,8 +13,11 @@ const Router: FC = () => (
     <Layout>
       <Routes>
         {/* <Route path="/" element={Home} /> */}
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/myfirstpage" element={<MyFirstPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/myfirstpage" element={<MyFirstPage />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+
       </Routes>
     </Layout>
   </BrowserRouter>
