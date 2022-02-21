6aa49ccdf3d8e8f96295b7c21a08e54df75fa32c
HerveMeste <herve.meste@gmail.com>
2022-02-21 18:27:48 +0100

    try to implement a new drawer in project

---
+++ src/components/navbar/NavBar2.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import {styled} from "@mui/material/styles";
+import MuiDrawer from "@mui/material/Drawer";
+
+
+const NavBar = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        '& .MuiDrawer-paper': {
+            position: 'relative',
+            whiteSpace: 'nowrap',
+            width: 240,
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            boxSizing: 'border-box',
+            ...(!open && {
+                overflowX: 'hidden',
+                transition: theme.transitions.create('width', {
+                    easing: theme.transitions.easing.sharp,
+                    duration: theme.transitions.duration.leavingScreen,
+                }),
+                width: theme.spacing(7),
+                [theme.breakpoints.up('sm')]: {
+                    width: theme.spacing(9),
+                },
+            }),
+        },
+    }),
+);
+
+export default NavBar;
