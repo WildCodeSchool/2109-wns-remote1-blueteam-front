cce681ed8ae9e4b9297001aff6c329b9e1253406
rickifox <ricardo.pereira.developpeur@gmail.com>
2022-02-23 11:26:27 +0100

    created theme folder and started taskList (unfinished)

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -3,19 +3,21 @@ import { Route, Routes } from 'react-router-dom';
 import SignIn from '../screens/signin/SignIn';
 import SignUp from '../screens/signup/SignUp';
 import Profile from '../screens/profile/Profile';
+import TaskList from '../screens/tasklist/TaskList';
 import Layout from '../components/layout/Layout';
 import TaskDetails from '../screens/taskDetails/TaskDetails';
 
 const Router: FC = () => (
-    <Layout>
-      <Routes>
-        {/* <Route path="/" element={Home} /> */}
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/taskDetails" element={<TaskDetails />} />
-      </Routes>
-    </Layout>
+  <Layout>
+    <Routes>
+      <Route path="/" element={<TaskList />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/taskdetails" element={<TaskDetails />} />
+      <Route path="/tasklist" element={<TaskList />} />
+    </Routes>
+  </Layout>
 );
 
 export default Router;
---
+++ src/theme/base.js
@@ -0,0 +1,5 @@
+import {StyleSheet} from 'react';
+
+const base = StyleSheet.create({
+    
+});
