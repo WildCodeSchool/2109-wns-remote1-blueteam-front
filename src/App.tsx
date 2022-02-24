b0a1959f096b13a7460b441a0d1fa98bd1663db9
benjamin <benjamin.d@make-it.fr>
2022-02-24 10:22:25 +0100

    refacto index and app fils & add apollo client provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,24 @@ import React, { FC } from 'react';
 import './App.css';
 import Router from './routes/Router';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+import { BrowserRouter } from 'react-router-dom';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5050/graphql',
+  cache: new InMemoryCache()
+});
+
 const App: FC = () => (
-  <>
-    <Router />
-  </>
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </ApolloProvider>
 );
 
 export default App;
